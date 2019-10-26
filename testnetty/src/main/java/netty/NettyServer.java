03460b9748e6e1253c875e086ce3bdafebc24ed7
fengxuanstone <651353997@qq.com>
2019-10-26 13:31:03 +0800

    [author]:feng xuan [msg]:test netty

---
+++ testnetty/src/main/java/netty/NettyServer.java
@@ -0,0 +1,8 @@
+package netty;
+
+public class NettyServer {
+    public static void main(String[] args) {
+        //这就是主要的服务启动器
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+    }
+}
